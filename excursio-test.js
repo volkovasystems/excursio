b236575824cb6083cc762f510678b91713c735f1
Vinse Vinalon <vinsevinalon@gmail.com>
2017-08-29 12:47:18 +0800

    Update client/server unit test.

--- excursio-test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const assert = require( "assert" );
-const excursio = require( "./excursio.js" );
-
-assert.equal( excursio.bind( { "hello": "world" } )( "1+1" ), 2 );
-
-console.log( "ok" );
